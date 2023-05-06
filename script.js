537d00c78cd370a4d96f6f06fc6207118a99245e
阿極 Aji <76050035+AJI0417@users.noreply.github.com>
2023-05-06 18:08:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function sendData() {
   const question = inputQuestion.value;
   send.disabled = true;
   inputQuestion.disabled = true;
-  const response = await fetch("https://tssh-openai-project-server.vercel.app/", {
+  const response = await fetch("https://tssh-openai-project-server.vercel.app", {
     method: "POST",
     mode: "cors",
     headers: {
