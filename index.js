f96f8416a60752c8c71423ef40e9559b079c65a5
阿極 Aji <76050035+AJI0417@users.noreply.github.com>
2023-05-06 17:27:05 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const form = document.getElementById("my-form");
+const answerDiv = document.getElementById("main");
+const userQuestion = document.getElementById("user-question");
+const botAnswer = document.getElementById("bot-answer");
+const inputQuestion = document.getElementById("question");
+const send = document.getElementById("send");
+
+inputQuestion.addEventListener("keydown", e => {
+  if (e.key == "Enter") {
+    sendData();
+  }
+});
+
+send.addEventListener("click", e => {
+  sendData();
+});
+
+async function sendData() {
+  inputQuestion.style.border = "0";
+  if (inputQuestion.value == "") {
+    inputQuestion.style.border = "1px solid #ff4346";
+    return false;
+  }
+  const question = inputQuestion.value;
+  send.disabled = true;
+  inputQuestion.disabled = true;
+  const response = await fetch("http://localhost:3000/", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ question }),
+  });
+  inputQuestion.value = "";
+  const data = await response.json();
+  let mainDiv = document.createElement("div");
+  mainDiv.className = "result";
+  mainDiv.innerHTML = `
+  <h2 id="user-message">Q: ${data.question}</h2>
+  <p class="bot-message">A: ${data.answer}</p>`;
+  answerDiv.appendChild(mainDiv);
+  send.disabled = false;
+  inputQuestion.disabled = false;
+}
